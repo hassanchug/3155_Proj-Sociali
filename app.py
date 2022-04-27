00fb9f65c2b7c5c825b05119bd1a32183a30f870
isteed2002 <ivory.steed2002@gmail.com>
2022-04-26 22:34:51 -0400

    added some template stuff to app.py and created the env file

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import os
 
+from dotenv import load_dotenv
+from flask import Flask, abort, redirect, render_template, request
+
+from src.models import db
+
+load_dotenv()
+
+app = Flask(__name__)
