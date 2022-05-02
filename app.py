7c65e499a31b3dfd2d22cebde47e36c69175d96a
isteed2002 <ivory.steed2002@gmail.com>
2022-05-01 21:34:38 -0400

    adding things to app.py

--- app.py
+++ app.py
@@ -4,7 +4,24 @@ from dotenv import load_dotenv
 from flask import Flask, abort, redirect, render_template, request
 
 from src.models import db
+from src.repositories.(our repository here) import (our repository)_singleton
 
 load_dotenv()
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('CLEARDB_DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.get('/login')
+def user_login():
+    return render_template('user_login.html')
+
+@app.get('/signup')
+def user_signup():
+    return render_template('user_signup.html')
+
+@app.get('/post_feed')
+def post_feed():
+    return render_template('post_feed.html')
