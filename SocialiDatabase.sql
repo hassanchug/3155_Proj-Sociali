d1800fb9255c4bd1e6dc15a8a348c637dcc076f0
Tony <tonyle626@gmail.com>
2022-05-06 16:40:27 -0400

    adding the full db

--- SocialiDatabase.sql
+++ SocialiDatabase.sql
@@ -9,6 +9,14 @@ CREATE TABLE IF NOT EXISTS user (
     user_password VARCHAR(255) NOT NULL,
     PRIMARY KEY (username_id)
 );
+CREATE TABLE IF NOT EXISTS replies (
+	reply_num INT AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    user VARCHAR(255) NOT NULL,
+    timeposted VARCHAR(255) NOT NULL,
+    likes INT NOT NULL,
+    PRIMARY KEY (reply_num)
+);
 
 CREATE TABLE IF NOT EXISTS posts (
 	post_id INT AUTO_INCREMENT NOT NULL,
@@ -22,5 +30,20 @@ CREATE TABLE IF NOT EXISTS likes (
 	like_num INT AUTO_INCREMENT NOT NULL,
     post VARCHAR(255) NOT NULL,
     PRIMARY KEY (like_num)
+);
+
+CREATE TABLE IF NOT EXISTS userposts (
+	Foreign Key (post_num), (user_num)
+    PRIMARY KEY (post_num), (user_num) 
+);
+
+CREATE TABLE IF NOT EXISTS userreplies (
+	Foreign Key (reply_num), (user_num)
+    PRIMARY KEY (reply_num), (user_num) 
+);
+
+CREATE TABLE IF NOT EXISTS postlikes (
+	Foreign Key (like_num), (post_num)
+    PRIMARY KEY (like_num), (post_num) 
 );
 
