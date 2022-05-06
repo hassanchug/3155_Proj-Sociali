76fd8ff98ddbbcae4c2f184e58d0f0c74546fe72
paulthottappilly1 <98353260+paulthottappilly1@users.noreply.github.com>
2022-05-05 21:10:09 -0400

    updating models.py

--- src/models.py
+++ src/models.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    username = db.Column(db.String, primary_key=True)
+    firstname = db.Column(db.String, nullable=False)
+    lastname = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
