93cebedfeee1a0ebccc12d8f8ae6824aa75e2243
isteed2002 <ivory.steed2002@gmail.com>
2022-05-06 15:44:11 -0400

    adding pages into app.py

--- app.py
+++ app.py
@@ -25,3 +25,19 @@ def user_signup():
 @app.get('/post_feed')
 def post_feed():
     return render_template('post_feed.html')
+
+@app.get('/user_profile')
+def user_profile():
+    return render_template('user_profile.html')
+
+@app.get('/create_post')
+def create_post():
+    return render_template('create_a_post.html')
+
+@app.get('/edit_post')
+def edit_post():
+    return render_template('edit_a_post.html')
+
+@app.get('/view_post')
+def view_post():
+    return render_template('view_individual_post.html')
