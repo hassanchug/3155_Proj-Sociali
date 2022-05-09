f467d70e0668f46bfc8152c8f11e96522319ebd7
paulthottappilly1 <98353260+paulthottappilly1@users.noreply.github.com>
2022-05-09 18:56:55 -0400

    Update models.py

--- src/models.py
+++ src/models.py
@@ -7,3 +7,10 @@ class User(db.Model):
     firstname = db.Column(db.String, nullable=False)
     lastname = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
+
+class Posts(db.Model):
+    post_id = db.Column(db.String, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    timepost = db.Column(db.String, nullable=False)
+    likes = db.Column(db.String, nullable=False)
