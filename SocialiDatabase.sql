4b84e80c37c9efd770a404b3aee514d8808d8ce8
Hassan Chughtai <hchughta@uncc.edu>
2022-05-10 19:12:44 -0400

    database fixed and deployed to heroku

--- SocialiDatabase.sql
+++ SocialiDatabase.sql
@@ -3,14 +3,14 @@ CREATE DATABASE IF NOT EXISTS SocialiApp;
 USE SocialiApp;
 
 CREATE TABLE IF NOT EXISTS users (
-	username_id INT AUTO_INCREMENT NOT NUll,
+	username_id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     user_password VARCHAR(255) NOT NULL,
     PRIMARY KEY (username_id)
 );
 CREATE TABLE IF NOT EXISTS replies (
-	reply_num INT AUTO_INCREMENT,
+	reply_num INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(255) NOT NULL,
     users VARCHAR(255) NOT NULL,
     timeposted VARCHAR(255) NOT NULL,
@@ -33,17 +33,26 @@ CREATE TABLE IF NOT EXISTS likes (
 );
 
 CREATE TABLE IF NOT EXISTS userposts (
-	Foreign Key (post_num), (username_id)
-    PRIMARY KEY (post_num), (username_id) 
+	post_num int NOT NULL, 
+    username_id int NOT NULL,
+    Foreign Key (post_num) REFERENCES posts(post_id), 
+    Foreign Key (username_id) REFERENCES users(username_id),
+    PRIMARY KEY (post_num, username_id) 
 );
 
 CREATE TABLE IF NOT EXISTS userreplies (
-	Foreign Key (reply_num), (username_id)
-    PRIMARY KEY (reply_num), (username_id) 
+	reply_num int NOT NULL, 
+    username_id int NOT NULL,
+    Foreign Key (reply_num) REFERENCES replies(reply_num), 
+    Foreign Key (username_id) REFERENCES users(username_id),
+    PRIMARY KEY (reply_num, username_id) 
 );
 
 CREATE TABLE IF NOT EXISTS postlikes (
-	Foreign Key (like_num), (username_id)
-    PRIMARY KEY (like_num), (username_id) 
+	like_num int NOT NULL, 
+    username_id int NOT NULL,
+    Foreign Key (like_num) REFERENCES likes(like_num), 
+    Foreign Key (username_id) REFERENCES users(username_id),
+    PRIMARY KEY (like_num, username_id)  
 );
 
