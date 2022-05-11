c42dfae6a3ec6508ab5b1b9ffb0ed0209a62e584
Hassan Chughtai <hchughta@uncc.edu>
2022-05-11 13:45:55 -0400

    Added methods in app and post repo

--- src/models.py
+++ src/models.py
@@ -3,14 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Users(db.Model):
-    username = db.Column(db.String, primary_key=True)
+    username_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     user_password = db.Column(db.String, nullable=False)
 
 class Posts(db.Model):
-    post_id = db.Column(db.String, primary_key=True)
+    post_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     username = db.Column(db.String, nullable=False)
     timepost = db.Column(db.String, nullable=False)
-    #likes = db.Column(db.Int, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
--- src/repositories/post_repository.py
+++ src/repositories/post_repository.py
@@ -3,21 +3,26 @@ from src.models import db
 class PostRepository:
 
     def get_all_posts(self):
-        return Users.query.all()
+        all_posts = Posts.query.all()
+        return all_posts
 
     def get_post_by_id(self, post_id):
-        return Users.query.get(post_id)
+        found_post = Posts.query.get_or_404(post_id)
+        return found_post
 
     def create_user(self, username_id, first_name, last_name, password):
-        new_user = Users(username_id='HotBoi', first_name='James', last_name='Charles', password='gay')
-        self._db.append(Users)
+        new_user = Users(username_id=username_id, first_name=first_name, last_name=last_name, password=password)
+        db.session.add(new_user)
+        db.session.commit()
         return new_user
 
     def search_posts(self, username_id):
-        print(('username').ilike('word'))
+        found_posts = Posts.query.filter(Users.username_id.ilike(f'%{username_id}%')).all()
+        print(found_posts)
+        return(found_posts)
 
     def create_post(self, post_id, title, replies, timepost, likes):
-        new_post = Posts(post_id='1', title='Cool pics', replies='lovely pics', timepost='3:40pm', likes='sixteen likes')
+        new_post = Posts(post_id=post_id, title=title, replies=replies, timepost=timepost, likes=likes)
         db.session.add(new_post)
         db.session.commit()
         return new_post  
